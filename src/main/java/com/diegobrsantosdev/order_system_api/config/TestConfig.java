4faf170c3a0e1edd88fcd3f602fb9dfb149d2f93
diegobrsantosdev <diegobrsantosdev@gmail.com>
2025-09-02 06:14:47 -0300

    OrderStatus enum

--- src/main/java/com/diegobrsantosdev/order_system_api/config/TestConfig.java
+++ src/main/java/com/diegobrsantosdev/order_system_api/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.diegobrsantosdev.order_system_api.config;
 
 import com.diegobrsantosdev.order_system_api.entities.Order;
 import com.diegobrsantosdev.order_system_api.entities.User;
+import com.diegobrsantosdev.order_system_api.enums.OrderStatus;
 import com.diegobrsantosdev.order_system_api.repositories.OrderRepository;
 import com.diegobrsantosdev.order_system_api.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Diego Br Santos", "diegobrrsant@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Bruno A Pereira", "brunoapereira@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/diegobrsantosdev/order_system_api/entities/Order.java
+++ src/main/java/com/diegobrsantosdev/order_system_api/entities/Order.java
@@ -1,5 +1,6 @@
 package com.diegobrsantosdev.order_system_api.entities;
 
+import com.diegobrsantosdev.order_system_api.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
@@ -12,7 +13,6 @@ import java.time.Instant;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"id"})
 @Table(name = "tb_orders")
@@ -28,8 +28,30 @@ public class Order implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    // Custom constructor accepting OrderStatus
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus); // uses custom setter
+        this.client = client;
+    }
+
+    // Custom Getter: convert Integer to Enum
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    // Custom setter: receives Enum and saves Integer
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode(); // uses getCode() of enum
+        }
+    }
 
 }
